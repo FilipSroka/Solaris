c613d453cf0abdf78dd27f464b42ab298837cc2b
Connor Watts <connorwatts@hotmail.com>
2024-02-19 15:05:35 +0000

    player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 50f; 
+    public float rotationSpeed = 2f; 
+    public float maxRotationAngle = 360f; 
+    public float slightForceAmount = 0.5f; // Adjust this for desired effect
+    private float targetPitch = 0;
+    private float targetRotationY = 0;
+
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() 
+    {
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            targetRotationY -= rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            targetRotationY += rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetPitch -= rotationSpeed; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            targetPitch += rotationSpeed; 
+        }
+        targetPitch = Mathf.Clamp(targetPitch, -maxRotationAngle, maxRotationAngle); 
+
+        // Apply Rotation
+        Quaternion targetRotation = Quaternion.Euler(targetPitch, targetRotationY, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5f); 
+
+        // Forward Movement (Immediately based on updated facing direction)
+        // Movement with Primarily Velocity & Slight Force
+        Vector3 targetVelocity = transform.forward * speed; 
+
+        // Slight Force Adjustment
+        rb.AddForce(transform.forward * slightForceAmount, ForceMode.Force);
+
+        // Apply most of the desired movement through velocity change
+        rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, Time.deltaTime * 5f); 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
